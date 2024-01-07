ebe0d9cea45b385e8040c295629bb86ada31faa1
vilnar <dwallin857@gmail.com>
2024-01-07 10:29:30 +0200

    add build step for update script

--- update.bat
+++ update.bat
@@ -1,6 +1,12 @@
-@echo off
+REM @echo off
 echo update...
 git pull origin master
 git log -1 --oneline
+echo build...
+go build -o ./bin/SERVER.exe quiz/cmd/server
+go build -o ./bin/setup.exe quiz/cmd/setup
+go build -o ./bin/dump-db.exe quiz/cmd/exportdb
+go build -o ./bin/import-db.exe quiz/cmd/importdb
 echo:
+echo Done!
 pause
